fcf0a582550b8e9bfea175f38bc098fb64100c4b
Nicolás Andrés Carvajal Jímenez <nicolascaji@unisabana.edu.co>
2020-04-08 04:22:01 -0500

    Genera claves privadas y publicas y las escribe en un archivo .pem, no sé como funciona el programa, ya que siempre genera las mismas claves

--- SeguridadRSA/src/seguridadrsa/FileShare.java
+++ SeguridadRSA/src/seguridadrsa/FileShare.java
@@ -1,9 +1,41 @@
 package seguridadrsa;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
+
 public class FileShare {
 
-    public static void main(String[] args) {
-        
+    PrivateKey PrivKey;
+    PublicKey PubKey;
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+        FileShare este = new FileShare();
+        este.crearClaves();
     }
-    
+
+    public void crearClaves() throws NoSuchAlgorithmException, IOException {
+        KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA"); //Crea la instancia del generador de keys y lo configura para que use RSA
+        keygen.initialize(1024); //Inicializa el generador en 1024 bits
+        KeyPair keypair = keygen.generateKeyPair(); //genera las keys
+
+        PrivKey = keypair.getPrivate(); //Obtiene la key privada
+        PubKey = keypair.getPublic(); //Obtiene la Key publica
+        
+        //Escribe la clave privada en un archivo .PEM, este se guarda en el directorio del proyecto
+        JcaPEMWriter writer = new JcaPEMWriter(new FileWriter(new File("PrivateKey.pem")));
+        writer.writeObject(PrivKey);
+        writer.close();
+        
+         //Escribe la clave publica en un archivo .PEM, este se guarda en el directorio del proyecto
+        writer = new JcaPEMWriter(new FileWriter(new File("PublicKey.pem")));
+        writer.writeObject(PubKey);
+        writer.close();
+    }
 }
